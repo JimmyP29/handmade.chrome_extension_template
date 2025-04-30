d762ac968452a6651e3c7f3010f8c2a7eccc3faf
James Privett <jamesprivett29@gmail.com>
2025-04-30 19:06:15 +0100

    background service worker set up

--- chrome_extension_template/vite.config.ts
+++ chrome_extension_template/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        background: "src/background/index.ts",
+        side_panel: "index.html",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+    outDir: "dist",
+  },
   plugins: [react()],
-})
+});
